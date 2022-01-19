68b6e1bf69c57498ee0f190a6ed92752d7871a47
pmpurcell93 <maddenpurcell@gmail.com>
2022-01-18 20:56:15 -0600

    Add project files.

---
+++ TheHeist/Models/TeamMember.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheHeist.Models
+{
+    internal class TeamMember
+    {
+        public TeamMember(string name, int skill, double courage)
+        {
+            Name = name;
+            SkillLevel = skill;
+            CourageFactor = courage;
+        }
+        public string Name { get; set; }
+        public int SkillLevel { get; set; }
+        public double CourageFactor { get; set; }
+
+        public void Print()
+        {
+            var output = ($@"
+_________________________
+Name: {Name}
+Skill Level: {SkillLevel}
+Courage: {CourageFactor}
+_________________________");
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ TheHeist/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using TheHeist.Models;
+
+namespace TheHeist
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Plan Your Heist!");
+
+            var question1 = "Enter the team member's name:";
+            var question2 = "Enter the team member's skill level";
+            var question3 = "Enter the team member's courage level";
+
+            int parsedSkill = 0;
+            double parsedCourage = 0;
+            bool successful = false;
+
+            Console.WriteLine(question1);
+            var name = Console.ReadLine();
+
+            while (!successful) {
+            Console.WriteLine(question2);
+            var skill = Console.ReadLine();
+            successful = int.TryParse(skill, out parsedSkill);
+            }
+
+            successful = false;
+
+            while (!successful) { 
+            Console.WriteLine(question3);
+            var courage = Console.ReadLine();
+            successful = double.TryParse(courage, out parsedCourage);
+            }
+
+            var teammate = new TeamMember(name, parsedSkill, parsedCourage);
+
+            teammate.Print();
+        }
+    }
+}
