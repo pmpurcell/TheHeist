3dc1159cb61e2cdf1c8eb6429b655101ca7f13e7
pmpurcell93 <maddenpurcell@gmail.com>
2022-01-22 14:03:01 -0600

    Completed phase 3

--- TheHeist/Program.cs
+++ TheHeist/Program.cs
@@ -8,7 +8,18 @@ namespace TheHeist
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Plan Your Heist!");
+//### Phase Three
+
+//            The program should be updated to...
+
+//1.Stop displaying each team member's information.
+//2.Store a value for the* bank's difficulty level*. Set this value to 100.
+//3.Sum the skill levels of the team.Save that number.
+//4.Compare the number with the bank's difficulty level. If the team's
+//skill level is greater than or equal to the bank's difficulty level,
+//Display a success message, otherwise display a failure message.
+
+           Console.WriteLine("Plan Your Heist!");
 
             var teamQuestion = "Enter your Heist Team's name";
             var question1 = "Enter the team member's name:";
@@ -17,11 +28,13 @@ namespace TheHeist
 
             int parsedSkill = 0;
             double parsedCourage = 0;
+            int teamSkill = 0; 
             bool successful = false;
 
             Console.WriteLine(teamQuestion);
             var teamName = Console.ReadLine();
             HeistTeam team = new HeistTeam(teamName);
+            Bank bank = new Bank();
 
             Console.WriteLine(question1);
             var name = Console.ReadLine();
@@ -47,6 +60,7 @@ namespace TheHeist
             var teammate = new TeamMember(name, parsedSkill, parsedCourage);
 
             team.AddTeamMember(teammate);
+            teamSkill += parsedSkill;
 
             Console.WriteLine();
             Console.WriteLine(question1);
@@ -54,6 +68,7 @@ namespace TheHeist
 
             }
             team.ShowTeam();
+            bank.RunHeist(teamSkill);
             }
         }
     }
